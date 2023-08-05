b123b6f3855ce208788d2033c49b45942be21bf3
Goldenmarquice <usmcmarquice@gmail.com>
2023-08-05 17:44:34 -0600

    task_mgr

---
+++ setup.sql
@@ -0,0 +1,30 @@
+
+
+-- Create task table
+
+
+CREATE TABLE task (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    summary VARCHAR(128),
+    description VARCHAR(256),
+    is_done BOOLEAN DEFAULT 0
+);
+
+-- Insert some dummy data:
+
+INSERT INTO task (
+    summary,
+    description
+)VALUES
+(
+    "Wash car",
+    "Take the car to the car wash and have it washed"
+),
+(
+    "Walk the dog",
+    "Take the dog out for a walk"
+),
+(
+    "Make dinner",
+    "Get groceries and prepare a meal"
+);
